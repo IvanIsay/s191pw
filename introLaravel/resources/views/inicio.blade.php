34661800cddf7f0e5f9e17e37f1b42fb9564f109
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-09-25 19:26:37 -0600

    add introlaravel Proyect

---
+++ introLaravel/resources/views/inicio.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    @vite(['resources/js/app.js'])
+
+    
+    <style>
+        body, html {
+            height: 100%; /* Asegura que el body tenga una altura de 100% */
+        }
+        .full-height {
+            height: 100vh; /* Altura completa de la ventana */
+        }
+    </style>
+</head>
+<body>
+
+    <div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
+        <h1 class="display-1">Bienvenido querido Turista!</h1>/
+        <p>Presiona el botón para iniciar....</p>
+        <button class="btn btn-primary">Comencemos!!</button>
+    </div>
+    
+   
+</body>
+</html>
