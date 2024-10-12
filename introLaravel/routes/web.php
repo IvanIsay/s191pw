7ab3c3468ecf86f41ba5bd3e8f98cef6f5add700
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-10-11 20:24:36 -0600

    practica 4 completa

--- introLaravel/routes/web.php
+++ introLaravel/routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
 
 
 
-/* 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* rutas para trabajar con el controlador */
 
-*/
+Route::get('/',[controladorVistas::class,'home' ])->name('rutainicio');
+Route::get('/form',[controladorVistas::class,'insert' ])->name('rutacacas');
+Route::get('/consultar',[controladorVistas::class,'select' ])->name('rutaconsulta');
 
-Route::view('/','inicio')->name('rutainicio');
+
+
+
+/* Route::view('/','inicio')->name('rutainicio');
 
 Route::view('/form','formulario')->name('rutacacas');
 
-Route::view('/consultar','clientes')->name('rutaconsulta');
+Route::view('/consultar','clientes')->name('rutaconsulta'); */
 
 Route::view('/component','componentes')->name('rutacomponent');
